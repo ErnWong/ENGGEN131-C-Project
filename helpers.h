962b11931119f732dedb079ffac2064efe7d82ee
Ernest Wong <ewon521@gmail.com>
2017-10-12 10:23:25 +1300

    Update helper function prototypes

--- helpers.h
+++ helpers.h
@@ -1,14 +1,15 @@
 int Gcd(int x, int y);
 int IsLowerAlphabetic(char c);
 char ToUpperCase(char c);
-int IsPrimeNaive(int p);
-int IsPrimeSeive(int p, int * isComposite);
-int IsPrime(int p, int * isComposite);
+void MazeInitLocations(int maze[MAZE_SIZE][MAZE_SIZE], int * r1, int * c1,
+		int * r2, int * c2)
 int Signum(int x);
+int IntMax(int * values, int size);
+char * CharRepeat(char * string, char c, int count);
 int IsGold(int r, int c, int map[MAX_MAP_SIZE][MAX_MAP_SIZE]);
 int IsPure(int r, int c, int map[MAX_MAP_SIZE][MAX_MAP_SIZE]);
 void GoldRush0(int * results, int rows, int cols,
-    int map[MAX_MAP_SIZE][MAX_MAP_SIZE]);
+		int map[MAX_MAP_SIZE][MAX_MAP_SIZE]);
 
 typedef struct DisjointSet DisjointSet;
 
@@ -17,8 +18,12 @@ void SetSwap(DisjointSet ** setA, DisjointSet ** setB);
 DisjointSet * SetFind(DisjointSet * set);
 void SetMerge(DisjointSet * setA, DisjointSet * setB);
 
-void ConnectCell(int r, int c, DisjointSet sets[MAX_MAP_SIZE][MAX_MAP_SIZE]);
+void ConnectCell(int r, int c, DisjointSet sets[PADDED_SIZE][PADDED_SIZE]);
 void IntSwap(int * a, int * b);
 void QuickSort(int * array, int size);
+void GoldRushInitSets(DisjointSet sets[PADDED_SIZE][PADDED_SIZE],
+		int rows, int cols, int map[MAX_MAP_SIZE][MAX_MAP_SIZE], int bonus);
+int ExtractRegions(int * results, DisjointSet sets[PADDED_SIZE][PADDED_SIZE],
+		int rows, int cols);
 void GoldRush12(int * results, int rows, int cols,
-    int map[MAX_MAP_SIZE][MAX_MAP_SIZE], int bonus);
+		int map[MAX_MAP_SIZE][MAX_MAP_SIZE], int bonus);
